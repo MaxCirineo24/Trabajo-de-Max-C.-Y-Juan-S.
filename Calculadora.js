08b8ad35715e5c8422920bc755190289a8e2adfd
MaxCirineo24 <109227028+MaxCirineo24@users.noreply.github.com>
2022-07-13 09:28:35 -0500

    Add files via upload

---
+++ Calculadora.js
@@ -0,0 +1,27 @@
+function Sumar(){
+    var x=parseInt(document.getElementById('valor1').value);
+    var y=parseInt(document.getElementById('valor2').value);
+        var resultado= x+y
+    alert('La suma es '+resultado);
+}        
+
+function Restar(){
+    var x=parseInt(document.getElementById('valor1').value);
+    var y=parseInt(document.getElementById('valor2').value);
+        var resultado= x-y
+    alert('La resta es '+resultado);
+}        
+
+function Multiplicar(){
+    var x=parseInt(document.getElementById('valor1').value);
+    var y=parseInt(document.getElementById('valor2').value);
+        var resultado= x*y
+    alert('La multiplicación es '+resultado);
+}        
+
+function Dividir(){
+    var x=parseInt(document.getElementById('valor1').value);
+    var y=parseInt(document.getElementById('valor2').value);
+        var resultado= x/y
+    alert('La División es '+resultado);
+}        
